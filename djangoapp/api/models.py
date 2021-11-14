cf83752c21743c5fbf5801c2d46f6fb1933ea71f
Kevin Ha <kevinha298@live.com>
2021-11-14 10:20:35 -0800

    Added Patient class in Model.py

---
+++ djangoapp/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+from django.core.validators import MinValueValidator
+
+# Create your models here.
+class Patient(models.Model):
+    mrn = models.PositiveIntegerField(primary_key=True, validators=[MinValueValidator(100), MaxValueValidator(999)])
+    name = models.CharField(max_length=100)
+    dob = models.DateField()
+
+    def __str__(self):
+        return f'{self.employeeID} - {self.employeeName}'
