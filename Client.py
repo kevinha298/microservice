e4b14d0cfa7a96d3b7451deade6b1fd7a45def7a
Kevin Ha <kevinha298@live.com>
2021-11-24 14:31:11 -0800

    Update Client.py file to be used for testing.

--- Client.py
+++ Client.py
@@ -1,5 +1,6 @@
 import requests
 import random, string 
+from datetime import datetime, timedelta
 
 def get_token():
     tokenEndPoint = f'{baseURL}/api/token/'
@@ -13,22 +14,21 @@ def get_data():
     header = {'Authorization': f'Bearer {get_token()}'}
     response = requests.get(getEndPoint, headers = header)
     emp_data = response.json()
-    for e in emp_data:
-        print(e)
+    return emp_data 
 
 #post(add) data
 def post_data(random_mrn, random_name, random_dob):
     postEndPoint = f'{baseURL}/api/member/'
     header = {'Authorization': f'Bearer {get_token()}'}
     data = {
-        'mrn': f'{random_mrn}', 'name': f'{random_name}', 'dob': 68 + seq
+        'mrn': f'{random_mrn}', 'name': f'{random_name}', 'dob': f'{random_dob}'
     }
     response = requests.post(postEndPoint, data = data, headers = header)
     print(response.text, response.status_code)
 
 #edit data
-def edit_data(mrn, name, ranking, dob):
-    editEndPoint = f'{baseURL}/api/member/{mrn}/'
+def edit_data(id, name, dob):
+    editEndPoint = f'{baseURL}/api/member/{id}/'
     header = {'Authorization': f'Bearer {get_token()}'}
     data = {
         'name': f'{name}', 'dob': {dob}
@@ -37,8 +37,8 @@ def edit_data(mrn, name, ranking, dob):
     print(response.text, response.status_code)
 
 #delete data
-def delete_data(mrn):
-    deleteEndPoint = f'{baseURL}/api/member/{mrn}/'
+def delete_data(id):
+    deleteEndPoint = f'{baseURL}/api/member/{id}/'
     header = {'Authorization': f'Bearer {get_token()}'}
     response = requests.delete(deleteEndPoint, headers = header)
     print(response.text, response.status_code)
@@ -46,15 +46,29 @@ def delete_data(mrn):
 def get_random_name(name_len):
     return ''.join(random.choice(string.ascii_lowercase) for i in range(name_len)).capitalize()
 
-def get_random_dob():
-    pass
+def get_random_dob(min_year=1900, max_year=datetime.today().year):
+    start = datetime(min_year, 1, 1)
+    years = max_year - min_year + 1
+    end = start + timedelta(days=365 * years)
+    dob = (start + (end - start) * random.random()).date()
+    return dob 
+
+def get_largest_member_id():
+    return get_data()[-1]['id'] if get_data() else -1
 
 if __name__ == "__main__":
     baseURL = 'http://127.0.0.1:8000'
     # print(get_token())
     random_mrn = random.randint(1000, 10000)
     random_name = f'{get_random_name(3)} {get_random_name(6)}'
-    random_dob = ''
-    post_data(random_mrn, random_name, random_dob)
-    get_data()
+    random_dob = get_random_dob()
+    #insert new data:
+    # post_data(random_mrn, random_name, random_dob)
+    #retrieve existing data:
+    for m in get_data():
+        print(m)
+    #update last memeber of existing data:
+    # if get_largest_member_id() > 0: edit_data(get_largest_member_id(), 'Bill Owens', '1988-09-28')
+    #delete last member of existing data:
+    # if get_largest_member_id() > 0: delete_data(get_largest_member_id())
 
